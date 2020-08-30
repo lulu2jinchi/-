3c7546a6d7952ef3426039479735517d4262f115
lulu2jinchi <349318806@qq.com>
2020-08-30 22:20:12 +0800

    version 1

--- script.js
+++ script.js
@@ -3,53 +3,92 @@ let duck2 = document.querySelector('.duck2');
 let toushiText = document.querySelector('.toushiText');
 let yzb;
 let xzb;
-let x1 = 948;
-let y1 = 530;
-let x2 = 1792;
-let y2 = 511;
+let dx1 = 0;
+let dy1 = 0;
+let dx2 = 0;
+let dy2 = 0;
+let littled = 5;
 
-
-function dist(clickx,clicky, fixx, fixy) {
-    // alert(Math.pow(clickx - fixx, 2));
-    // alert(Math.pow(clicky - fixy, 2));
-    // alert(Math.sqrt(
-    //     Math.pow(clickx - fixx, 2)+Math.pow(clicky - fixy, 2)
-    // ));
-    return Math.sqrt(
-        Math.pow(clickx - fixx, 2)+Math.pow(clicky - fixy, 2)
-    );
-    
+function dist(clickx, clicky, x, y) {
+    return (Math.sqrt(Math.pow(clickx - x, 2) + Math.pow(clicky - y, 2)));
 }
 
+function sin(clickx, clicky, x, y, littled) {
+    let xiebian = dist(clickx, clicky, x, y);
+    let sinx = (clicky - y) / xiebian;
+    return littled * sinx;
+}
+function cos(clickx, clicky, x, y, littled) {
+    let xiebian = dist(clickx, clicky, x, y);
+    let cosx = (clickx - x) / xiebian;
+    return littled * cosx;
+}
+
+let interval = 0;
 
 function zuobiao(event) {
     xzb = event.clientX;
     yzb = event.clientY;
-    let distance1 = dist(xzb, yzb, x1, y1);
-    // alert(distance1);
-    let distance2 = dist(xzb, yzb, x2, y2);
-    let duration = 100;
-    let f = () => {
-        if (dist(xzb, yzb, duck1.offsetTop, duck1.offsetLeft) > 100) {
-            // alert(dist(xzb, yzb, duck1.offsetTop, duck1.offsetLeft)+','+xzb+","+yzb+","+duck1.offsetLeft+','+duck1.offsetTop ); 
-            setTimeout(
-                () => {
-                    let dx1 = 0;
-                    let dy1 = 0;
 
-                    dx1 = 5 * (xzb - x1) / distance1;
-                    dy1 = 5 * (yzb - y1) / distance1;
+    function move() {
+        dx1 = cos(xzb, yzb, duck1.offsetLeft, duck1.offsetTop, littled);
+        dy1 = sin(xzb, yzb, duck1.offsetLeft, duck1.offsetTop, littled);
+        dx2 = cos(xzb, yzb, duck2.offsetLeft, duck2.offsetTop, littled);
+        dy2 = sin(xzb, yzb, duck2.offsetLeft, duck2.offsetTop, littled);
 
-                    duck1.style.top = (duck1.offsetTop + dy1) + 'px';
-                    duck1.style.left = (duck1.offsetLeft + dx1) + 'px';
+        console.log(Math.acos(dx1 / littled) * 180 / Math.PI);
 
-                    // f();
-                }, 50
-            );
+        if (dist(xzb, yzb, duck1.offsetLeft, duck1.offsetTop) > 20) {
+            duck1.style.top = (duck1.offsetTop + dy1) + 'px';
+            duck1.style.left = (duck1.offsetLeft + dx1) + 'px';
+        }
+        if (dist(xzb, yzb, duck2.offsetLeft, duck2.offsetTop) > 20) {
+            duck2.style.top = (duck2.offsetTop + dy2) + 'px';
+            duck2.style.left = (duck2.offsetLeft + dx2) + 'px';
+        }
+
+        if (dist(xzb, yzb, duck1.offsetLeft, duck1.offsetTop) <= 20 && dist(xzb, yzb, duck2.offsetLeft, duck2.offsetTop) <= 20) {
+            clearInterval(interval);
         }
     }
-    f();
+    if(interval > 0) {
+        clearInterval(interval);
+    }
+    interval = setInterval(
+        ()=>{
+            move();
+        }, 50
+    );
 }
+
+// zuobiao();
+
+// function sleep(n) {
+//     let start = new Date().getTime();
+//     while (true) {
+//         var time = new Date().getTime();
+//         if (time - start > n) {
+//             break;
+//         }
+//     }
+// }
+// if (dist(xzb, yzb, duck2.offsetTop, duck2.offsetLeft) > 10) {
+    //     setTimeout(
+        //         () => {
+            //             let dx = 0;
+            //             let dy = 0;
+            
+            //             dx = 5 * cos(xzb, yzb, duck2.offsetLeft, duck2.offsetTop);
+            //             dy = 5 * sin(xzb, yzb, duck2.offsetLeft, duck2.offsetTop);
+            
+            //             duck2.style.top = (duck2.offsetTop + dy) + 'px';
+            //             duck2.style.left = (duck2.offsetLeft + dx) + 'px';
+            
+            //             f();
+//         }, 50
+//     );
+// }
+
 
 function toushi(event) {
     xzb1 = event.clientX;
@@ -66,14 +105,3 @@ function toushi(event) {
         toushiText.innerHTML = "投食";
     }
 }
-
-// document.onkeydown = checkKey;
-
-// function checkKey(e) {
-//     e.preventDefault();
-//     if (e.keyCode == '13') {
-//        while(xzb>948){
-//            duck1.style.left="1300px";
-//        }
-//     }
-// }
